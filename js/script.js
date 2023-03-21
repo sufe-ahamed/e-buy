007d6f8a517fbc70b2b8f9c8344e3dfcfe182d0a
Jotey Properties <joteyproperties2023@gmail.com>
2023-03-21 21:55:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$('.banner_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<i class="fa-solid fa-angle-right prev "></i>',
+    nextArrow: '<i class="fa-solid fa-angle-left next"></i>',
+    dots: true,
+  });
